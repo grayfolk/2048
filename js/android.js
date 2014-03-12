c7113802b2f4531a645430d82b08495faf2d0ef3
grayfolk <grayfolk@gmail.com>
2014-03-12 21:22:38 +0200

    Test

--- js/android.js
+++ js/android.js
@@ -1,5 +1,18 @@
 navigator.notification.vibrate(500);
-alert (123);
-function vibrate(){
+
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
 	navigator.notification.vibrate(500);
-}
+};
+
+function onError() {
+    alert('onError!');
+};
+
+var options = { frequency: 100 };  // Update every 3 seconds
+
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
