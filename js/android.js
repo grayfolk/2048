80bd13cc1022f7e022d9edc598de25e8dd0cb1bc
grayfolk <grayfolk@gmail.com>
2014-03-12 21:24:25 +0200

    Test

--- js/android.js
+++ js/android.js
@@ -1,18 +1,49 @@
 navigator.notification.vibrate(500);
 
+var watchID = null;
 
-function onSuccess(acceleration) {
-    alert('Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
-	navigator.notification.vibrate(500);
-};
+    // Wait for Cordova to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-function onError() {
-    alert('onError!');
-};
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+        startWatch();
+    }
 
-var options = { frequency: 100 };  // Update every 3 seconds
+    // Start watching the acceleration
+    //
+    function startWatch() {
 
-var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+        // Update acceleration every 3 seconds
+        var options = { frequency: 100 };
+
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+    }
+
+    // Stop watching the acceleration
+    //
+    function stopWatch() {
+        if (watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+        }
+    }
+
+    // onSuccess: Get a snapshot of the current acceleration
+    //
+    function onSuccess(acceleration) {
+        var element = document.getElementById('accelerometer');
+        element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+                            'Acceleration Y: ' + acceleration.y + '<br />' +
+                            'Acceleration Z: ' + acceleration.z + '<br />' +
+                            'Timestamp: '      + acceleration.timestamp + '<br />';
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        alert('onError!');
+    }
+
