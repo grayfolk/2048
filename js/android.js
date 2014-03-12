1ee346c67670b5a67aefc72665cb961244b71698
grayfolk <grayfolk@gmail.com>
2014-03-12 21:33:59 +0200

    Fix

--- js/android.js
+++ js/android.js
@@ -1,30 +1,21 @@
 var app = {
-    // Application Constructor
+	watchID: null,
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
     receivedEvent: function(id) {
+		alert (id);
 		navigator.notification.vibrate(500);
-		alert (123123);
     }
 };
 
-var watchID = null;
+/*var watchID = null;
 
     // Wait for Cordova to load
     //
@@ -71,3 +62,4 @@ var watchID = null;
         alert('onError!');
     }
 
+*/
