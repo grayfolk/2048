a21f8b0b72bb4d649e926e98ea7084439177e6be
grayfolk <grayfolk@gmail.com>
2014-03-12 22:16:44 +0200

    test

--- js/android.js
+++ js/android.js
@@ -3,42 +3,29 @@ var watchID = null;
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady(){
-	// startWatch();
+	startWatch();
 	navigator.notification.vibrate(500);
 	window.requestAnimationFrame(function () {
 		new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 	});
 }
-
-    function startWatch() {
-
-        // Update acceleration every 3 seconds
-        var options = { frequency: 100 };
-
-        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-    }
-
-    // Stop watching the acceleration
-    //
-    function stopWatch() {
-        if (watchID) {
-            navigator.accelerometer.clearWatch(watchID);
-            watchID = null;
-        }
-    }
-
-    // onSuccess: Get a snapshot of the current acceleration
-    //
-    function onSuccess(acceleration) {
-        var element = document.getElementById('accelerometer');
-        element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+function startWatch() {
+	var options = { frequency: 100 };
+	watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
+function stopWatch() {
+	if (watchID) {
+		navigator.accelerometer.clearWatch(watchID);
+		watchID = null;
+	}
+}
+function onSuccess(acceleration) {
+	var element = document.getElementById('accelerometer');
+	element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
                             'Acceleration Y: ' + acceleration.y + '<br />' +
                             'Acceleration Z: ' + acceleration.z + '<br />' +
                             'Timestamp: '      + acceleration.timestamp + '<br />';
-    }
-
-    // onError: Failed to get the acceleration
-    //
-    function onError() {
-        alert('onError!');
-    }
+}
+function onError() {
+	alert('onError!');
+}
