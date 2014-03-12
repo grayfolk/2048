efcd5f679696eec149f72f6798f31d8e27ac3edb
grayfolk <grayfolk@gmail.com>
2014-03-12 22:32:05 +0200

    test

--- js/android.js
+++ js/android.js
@@ -1,31 +1,7 @@
-var watchID = null;
-
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady(){
-	startWatch();
-	navigator.notification.vibrate(500);
 	window.requestAnimationFrame(function () {
 		new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 	});
-}
-function startWatch() {
-	var options = { frequency: 100 };
-	watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-}
-function stopWatch() {
-	if (watchID) {
-		navigator.accelerometer.clearWatch(watchID);
-		watchID = null;
-	}
-}
-function onSuccess(acceleration) {
-	var element = document.getElementById('accelerometer');
-	element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
-                            'Acceleration Y: ' + acceleration.y + '<br />' +
-                            'Acceleration Z: ' + acceleration.z + '<br />' +
-                            'Timestamp: '      + acceleration.timestamp + '<br />';
-}
-function onError() {
-	alert('onError!');
 }
